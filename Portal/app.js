618d39cc7c63c94fde8f7ac9c35e435262789d7d
unknown <ffeitosa.leonardo@gmail.com>
2021-09-21 12:37:31 -0300

    initial

---
+++ Portal/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.send("<html><body>Portal</body></html>");
+});
+
+app.get('/tecnology', function (req, res) {
+    res.render("section/tecnology");
+});
+
+app.get('/games', function (req, res) {
+    res.send("<html><body>Portal de games</body></html>");
+});
+
+app.get('/news', function (req, res) {
+    res.send("<html><body>Portal de noticias</body></html>");
+});
+
+app.listen(3000, function () {
+    console.log("express ok");
+});
