c4c02903f58a93053ab7c9dc954c16d9a1699166
unknown <ffeitosa.leonardo@gmail.com>
2021-09-29 12:39:59 -0300

    add validator

--- Portal/config/dbConnection.js
+++ Portal/config/dbConnection.js
@@ -1,7 +1,6 @@
 var mysql = require('mysql');
 
 var connMySQL = function () {
-    console.log('Conexao com db foi estabelecida');
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
@@ -11,6 +10,5 @@ var connMySQL = function () {
 }
 
 module.exports = function () {
-    console.log('Autoload carregou conexao');
     return connMySQL;
 }
--- Portal/config/server.js
+++ Portal/config/server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var consign = require('consign');
+var expressValidator = require('express-validator');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+app.use(express.urlencoded({ extended: true })); //get data to forms
+app.use(expressValidator());
 
 consign()
     .include('app/routes') //include routes automatic
