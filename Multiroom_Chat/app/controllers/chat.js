9c560d9f62bf6cd7373f04c45db562b50a581544
unknown <ffeitosa.leonardo@gmail.com>
2021-10-07 12:20:41 -0300

    validation

--- Multiroom_Chat/app/controllers/chat.js
+++ Multiroom_Chat/app/controllers/chat.js
@@ -1,3 +1,14 @@
 module.exports.runChat = function (application, req, res) {
+    var dataForm = req.body;
+    req.assert('apelido', 'O apelido é obrigatório.').notEmpty();
+    req.assert('apelido', 'O apelido deve ter entre 3 e 15 caracteres.').len(3, 15);
+
+    var errors = req.validationErrors();
+    if (errors) {
+        res.render('index', { validation: errors });
+        return;
+    }
+
+    console.log(dataForm);
     res.render('chat');
 }
